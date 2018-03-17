01b83916eb5e571f985e08de7c135b047eca59be
Horia Coman <horia141@gmail.com>
2018-03-17 17:27:34 +0200

    A refactoring of the configuration

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 module.exports = {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.COMMON_DATABASE_URL,
     pool: {
-	      min: 2,
-	      max: 10
+        min: 2,
+        max: 10
     },
     migrations: {
-	      directory: process.env.DATABASE_MIGRATIONS_DIR,
-	      tableName: process.env.DATABASE_MIGRATIONS_TABLE
+        directory: process.env.COMMON_DATABASE_MIGRATIONS_DIR,
+        tableName: process.env.COMMON_DATABASE_MIGRATIONS_TABLE
     }
 }
--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,24 @@
 import { Env, parseEnv, isOnServer } from '@truesparrow/common-js'
 import { getFromEnv } from '@truesparrow/common-server-js'
 
+// Common to all services
+
+export const ENV: Env = parseEnv(getFromEnv('COMMON_ENV'));
+
+export const DATABASE_URL: string = getFromEnv('COMMON_DATABASE_URL');
+export const DATABASE_MIGRATIONS_DIR: string = getFromEnv('COMMON_DATABASE_MIGRATIONS_DIR');
+export const DATABASE_MIGRATIONS_TABLE: string = getFromEnv('COMMON_DATABASE_MIGRATIONS_TABLE');
+export const IDENTITY_SERVICE_HOST: string = getFromEnv('COMMON_IDENTITY_SERVICE_HOST');
+
+export const LOGGLY_TOKEN: string | null = isOnServer(ENV) ? getFromEnv('COMMON_LOGGLY_TOKEN') : null;
+export const LOGGLY_SUBDOMAIN: string | null = isOnServer(ENV) ? getFromEnv('COMMON_LOGGLY_SUBDOMAIN') : null;
+export const ROLLBAR_TOKEN: string | null = isOnServer(ENV) ? getFromEnv('COMMON_ROLLBAR_TOKEN') : null;
+
+// Specific to content service
+
 export const NAME: string = 'content';
-export const ENV: Env = parseEnv(getFromEnv('ENV'));
-export const ADDRESS: string = getFromEnv('ADDRESS');
-export const PORT: number = parseInt(getFromEnv('PORT'), 10);
-export const DATABASE_URL: string = getFromEnv('DATABASE_URL');
-export const DATABASE_MIGRATIONS_DIR: string = getFromEnv('DATABASE_MIGRATIONS_DIR');
-export const DATABASE_MIGRATIONS_TABLE: string = getFromEnv('DATABASE_MIGRATIONS_TABLE');
-export const ORIGIN: string = getFromEnv('ORIGIN');
-export const CLIENTS: string[] = getFromEnv('CLIENTS').split(',');
-export const IDENTITY_SERVICE_HOST: string = getFromEnv('IDENTITY_SERVICE_HOST');
+export const ADDRESS: string = getFromEnv('CONTENT_ADDRESS');
+export const PORT: number = parseInt(getFromEnv('CONTENT_PORT'), 10);
+export const ORIGIN: string = getFromEnv('CONTENT_ORIGIN');
 
-export let LOGGLY_TOKEN: string | null;
-export let LOGGLY_SUBDOMAIN: string | null;
-export let ROLLBAR_TOKEN: string | null;
-
-if (isOnServer(ENV)) {
-    LOGGLY_TOKEN = getFromEnv('LOGGLY_TOKEN');
-    LOGGLY_SUBDOMAIN = getFromEnv('LOGGLY_SUBDOMAIN');
-    ROLLBAR_TOKEN = getFromEnv('ROLLBAR_TOKEN');
-} else {
-    LOGGLY_TOKEN = null;
-    LOGGLY_SUBDOMAIN = null;
-    ROLLBAR_TOKEN = null;
-}
+export const CLIENTS: string[] = getFromEnv('CONTENT_CLIENTS').split(',');
