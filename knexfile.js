3db614ac8eb73e6bdc559bbec79d864a9288926b
Horia Coman <horia141@gmail.com>
2018-03-19 12:09:50 +0200

    Update knexfile with new constant names

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 module.exports = {
     client: 'pg',
-    connection: process.env.COMMON_DATABASE_URL,
+    connection: process.env.CONTENT_DATABASE_URL,
     pool: {
         min: 2,
         max: 10
     },
     migrations: {
-        directory: process.env.COMMON_DATABASE_MIGRATIONS_DIR,
-        tableName: process.env.COMMON_DATABASE_MIGRATIONS_TABLE
+        directory: process.env.CONTENT_DATABASE_MIGRATIONS_DIR,
+        tableName: process.env.CONTENT_DATABASE_MIGRATIONS_TABLE
     }
 }
